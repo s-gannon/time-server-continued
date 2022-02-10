a08c5d04548bf47a0a540e703641dc84b60a028f
Spencer Gannon <spencer.gannon@valpo.edu>
2022-02-09 20:26:08 -0600

    Setup general structure

--- get_time.py
+++ get_time.py
@@ -1,4 +1,5 @@
 from gps import *
+import time
 
 #returns 2-tuple with latitude and longitude (both floats)
 def get_longitude_latitude():
@@ -9,4 +10,16 @@ def get_longitude_latitude():
         return lat_lon
 
 #takes in 2-tuple with latitude and longitude and returns the time at that location
-def get_time(lat_lon):
+def get_time_with_loc(lat_lon):
+    pass
+
+def get_time_with_rtc():
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        print("Node interrupted by KeyboardInterrupt")
