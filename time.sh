2cf947b98e156a6abf489a376fbdbbc6c7117eb1
Nathan Harmon <22229517+nathanharmon1@users.noreply.github.com>
2022-03-29 13:32:31 -0500

    organize file structure and clean time script

--- time.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-parse_unix() {
-		date -d "@$1" "+%H:%M:%S"
-}
-
-solar_time=$(./solar -t $1)
-
-echo $(parse_unix $1) $(parse_unix $solar_time) $(date -d "@$1" "+%m/%d/%Y") > ~/.cache/campanile_time
