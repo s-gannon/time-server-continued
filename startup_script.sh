aa5c10162df10c8933c57b6cd82d9991c7611be9
Spencer Gannon <spencer.gannon@valpo.edu>
2022-07-21 11:21:36 -0500

    Added functionality and edited html

--- startup_script.sh
+++ startup_script.sh
@@ -3,3 +3,5 @@
 sshfs pi@campanile-node-1:/home/pi/time-server/data /home/pi/time-data/node1
 sshfs pi@campanile-node-2:/home/pi/time-server/data /home/pi/time-data/node2
 sshfs pi@campanile-node-3:/home/pi/time-server/data /home/pi/time-data/node3
+
+go run /home/pi/time-server/clock/server.go
